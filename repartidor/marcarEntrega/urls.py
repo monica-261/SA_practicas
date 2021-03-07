abcd220da5ed2497a0717759ed266092a893418b
Mauro <monicalderon26@gmail.com>
2021-03-06 19:15:29 -0600

    updateMain

--- repartidor/marcarEntrega/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import marcarEntrega
-
-# Se declara la ruta por la cual se va a realizar el request GET 
-urlpatterns = [
-    path('marcarEntrega', marcarEntrega, name="marcarEntrega"),
-    
-]
