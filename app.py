7fbe4e9d2fc4bcd5d6820eeed79dc321292517a6
monica-261 <monicalderon26@gmail.com>
2021-04-05 21:56:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from flask import request, render_template, url_for
 import pymysql
 
 app = Flask(__name__)
-db = pymysql.connect(host="34.66.230.96",user="root",password="example",db="practica8")
+db = pymysql.connect(host="34.72.13.19",user="root",password="example",db="P8")
 
-@app.route('/api/practica8',methods=['GET'])
+@app.route('/api/practica',methods=['GET'])
 def cantidad():
     cursor = db.cursor()
-    sql = "SELECT * FROM tabla1"
+    sql = "SELECT * FROM DATOS_P8"
     cursor.execute(sql)
     results = cursor.fetchall()
     dic = {}
     for row in results:
         dic[row[0]]={}
-        dic[row[0]]["cadena1"]=row[1]
-        dic[row[0]]["cadena2"] = row[2]
+        dic[row[0]]["nombre"]=row[1]
+        dic[row[0]]["apellido"] = row[2]
     return jsonify(data=dic)
 
 
